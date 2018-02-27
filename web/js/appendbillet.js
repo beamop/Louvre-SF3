dbe4c5bc821cc83774349a550549f88a42c29457
beamop <beamop@null.net>
2018-02-27 07:19:19 +0100

    appendbillet.js|codacy FIX

--- web/js/appendbillet.js
+++ web/js/appendbillet.js
@@ -1,13 +1,11 @@
-var $container = $('div#form');
+var $container = $("div#form");
 var index = 1;
 
 function addBillet($container) {
     index++;
-    var template = $container.attr('data-prototype').replace(/__name__/g, index);
+    var template = $container.attr("data-prototype").replace(/__name__/g, index);
     var $prototype = $(template);
 
     $container.append($prototype);
-
-    console.log(index); //dev
 }
 
